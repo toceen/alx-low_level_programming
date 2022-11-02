9ab419a91c1236586c206d6fafa492b2334f0ba5
toceen <kitiborbor@gmail.com>
2022-11-02 16:28:16 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev recursion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
@@ -9,6 +9,6 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s)
+		_putchar(*s);
 	}
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string
+ * @s: the string to be measured
+ * Return: the lenght of the string
+ */
+int _strlen_recurssion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+}
