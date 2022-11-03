67052a05dea404188007833a63eb0d91717a9fa5
toceen <kitiborbor@gmail.com>
2022-11-03 04:19:29 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,3 +23,20 @@ int _evaluate(int i, int n)
 
 	return (-1);
 }
+
+/**
+ * _sqrt_recursion - evaluation sqrt
+ * @n: integer
+ * Return: Sgrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0) /*if n is negative*/
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n)); /*Recursive call*/
+	}
