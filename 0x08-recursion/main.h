3c1155aacace9da25e06aea1c46d0568c18037ad
toceen <kitiborbor@gmail.com>
2022-11-03 06:19:45 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,7 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _evaluate(int i, int n);
-int find_sqrt(int num, int root);
 
 #endif
