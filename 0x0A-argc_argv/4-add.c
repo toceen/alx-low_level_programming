92e7ae5350471cb5ac2c36cf0cc95d7ddfb205fc
toceen <kitiborbor@gmail.com>
2022-11-07 10:24:15 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,7 +7,7 @@
  * check_num - check - string there are digit
  * @str: array str
  *
- * Return:Always 0 (success)
+ * Return: Always 0 (success)
  */
 int check_num(char *str)
 {
@@ -26,4 +26,40 @@ int check_num(char *str)
 		count++;
 	}
 	return (1);
+}
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
