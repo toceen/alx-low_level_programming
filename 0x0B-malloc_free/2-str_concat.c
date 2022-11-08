c1b693c9c9e0271e1c9939ddaf2d1b4b09e4557c
toceen <kitiborbor@gmail.com>
2022-11-08 05:38:17 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,6 +33,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
+	if (s2 == NULL)
+		s2 = "";
+
 	size = (_strlen(s1) + _strlen(s2) + 1);
 
 	dst = (char *) malloc(size * sizeof(char));
