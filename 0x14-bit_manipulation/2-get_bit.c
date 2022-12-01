fc5501e27703d9245f0738eaa84897d12affe52c
toceen <kitiborbor@gmail.com>
2022-12-01 06:21:03 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,18 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) + 0))
-		return (-1);
+	unsigned int i;
 
-	if ((n & (1 << index)) == 0)
+	if (n == 0 && index < 64)
 		return (0);
 
-	return (1);
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
